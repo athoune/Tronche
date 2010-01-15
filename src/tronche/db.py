b2670e41fabe51a03a9b944175a566a2cad511dd
Mathieu Lecarme <mathieu@garambrogne.net>
2010-01-15 19:34:15 +0100

    iterator

--- src/tronche/db.py
+++ src/tronche/db.py
@@ -9,13 +9,22 @@ class Collectd(object):
 	def __init__(self, path):
 		self.path = path
 		self.domains = {}
+		for d in os.listdir(os.path.join(path, 'rrd')):
+			self.domains[d] = Domain(os.path.join(path, 'rrd', d))
+	def __iter__(self):
+		for d in self.domains.keys():
+			yield self.domains[d]
 
 class Domain(object):
 	def __init__(self, path):
 		self.path = path
 		self.sondes = {}
+		_, self.name = os.path.split(path)
 		for s in os.listdir(path):
 			self.sondes[s] = Sonde(os.path.join(path, s))
+	def __iter__(self):
+		for s in self.sondes.keys():
+			yield self.sondes[s]
 
 class Sonde(object):
 	def __init__(self, path):
@@ -23,15 +32,18 @@ class Sonde(object):
 		self.folder, self.name = os.path.split(path)
 		self.rrds = []
 		for r in os.listdir(path):
-			name, ext = os.path.splitext(path)
+			name, ext = os.path.splitext(self.name)
 			self.rrds.append(name)
 	def rrd(self, key):
 		return RRD(os.path.join(self.path, "%s.rrd" % key), mode='r')
 	def __repr__(self):
 		return "<Sonde %s>" %self.name
+	def __iter__(self):
+		for r in self.rrds:
+			yield self.rrd(r)
 
 if __name__ == '__main__':
-	d = Domain('collectd/collectd/rrd/localhost.localdomain')
-	#print d.sondes
-	r = d.sondes['load'].rrd('load')
-	print r.getData()
+	for domain in Collectd('collectd/collectd/'):
+		#print domain.sondes
+		for r in domain.sondes['load']:
+			print r.getData()
