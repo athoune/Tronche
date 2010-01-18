07e9868bbf0bb37c01edec0ce6986f3c6c5c30a0
Mathieu Lecarme <mathieu@garambrogne.net>
2010-01-18 14:09:23 +0100

    return python object

--- src/tronche/rrd.py
+++ src/tronche/rrd.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from subprocess import Popen, PIPE
+from datetime import datetime
 
 class Query(object):
 	def __init__(self, consolidation, path):
@@ -18,7 +19,7 @@ class Query(object):
 	def end(self, end):
 		self._end = end
 		return self
-	def execute(self):
+	def execute(self, column = 0):
 		buff = 'rrdtool fetch %s %s' % (self.path, self.consolidation)
 		if self._resolution != None:
 			buff += ' -r %i' % self._resolution
@@ -26,7 +27,7 @@ class Query(object):
 			buff += ' -s %s' % self._start
 		if self._end != None:
 			buff += ' -e %s' % self._end
-		return Popen(buff, shell=True, stdout=PIPE).stdout
+		return Result(Popen(buff, shell=True, stdout=PIPE).stdout, column)
 
 def AVERAGE(path = None):
 	return Query('AVERAGE', path)
@@ -41,10 +42,30 @@ class RRD(object):
 	def __init__(self, path):
 		self.path = path
 	def fetch(self, query):
-		return Popen('rrdtool fetch %s %s' % (self.path, query), shell=True, stdout=PIPE).stdout
+		"""[FIXME] LC_NUMERIC doesn't works 
+		"""
+		env = os.environ
+		env['LC_NUMERIC'] = 'en_US'
+		return Popen('rrdtool fetch %s %s' % (self.path, query), shell=True, env= env, stdout=PIPE).stdout
 	def query(self, query):
 		query.path = self.path
 		return query
+class Result(object):
+	def __init__(self, raw, column = 0):
+		self.raw = raw
+		self.column = column
+	def __iter__(self):
+		cpt = 0
+		for line in self.raw:
+			cpt += 1
+			if cpt > 2:
+				ts, values = line[:-1].split(': ',1)
+				value = values.split(' ')[self.column]
+				if value == 'nan':
+					yield (datetime.fromtimestamp(int(ts)),  None)
+				else:
+					yield (datetime.fromtimestamp(int(ts)), float(value))
+
 
 if __name__ == '__main__':
 	import time
@@ -53,7 +74,6 @@ if __name__ == '__main__':
 	for domain in os.listdir('collectd/collectd/rrd/') :
 		print domain
 		r = RRD('collectd/collectd/rrd/%s/load/load.rrd' % domain)
-		for line in r.query(AVERAGE().resolution(10).start('-10m')).execute():
-			print line
+		for ts, value in r.query(AVERAGE().resolution(10).start('-10m')).execute(0):
+			print ts, value
 	print (time.time() -chrono) *1000 , 'ms'
-	
