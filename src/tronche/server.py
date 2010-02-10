9be788cda44b54fa7181e57ae998d0e60c68de26
Mathieu Lecarme <mathieu@garambrogne.net>
2010-02-10 21:48:47 +0100

    flot + bottle

--- src/tronche/server.py
+++ src/tronche/server.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
-from wsgiref.simple_server import make_server
-from wsgiref.util import request_uri, FileWrapper
-from urlparse import urlparse, parse_qs
-import json
-import os.path
-import select
-import urllib
-from cStringIO import StringIO
+from bottle import route, run, view, send_file
 
 __author__ = "mlecarme"
 __version__ = "0.1"
+
+@route('/')
+@view('index')
+def index():
+    return ''
+
+@route('/data/:filename')
+def static(filename):
+	send_file(filename, root='./data')
+
+run(host='localhost', port=8080)
---
+++ src/tronche/views/index.tpl
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Tronche</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js" type="text/javascript"></script>
+	<link type="text/css" rel="stylesheet" href="data/style.css" />
+</head>
+<body>
+	<div>
+		<h1>Tronche</h1>
+	Beuha
+	</div>
+</body>
+</html>
