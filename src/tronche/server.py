88429b72e99e2afbb92e46a185e9efc73db9a0b3
Mathieu Lecarme <mathieu@garambrogne.net>
2010-02-10 23:03:00 +0100

    basic browse

--- src/tronche/server.py
+++ src/tronche/server.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 
 from bottle import route, run, view, send_file
+from collectd import Collectd
 
 __author__ = "mlecarme"
 __version__ = "0.1"
+
+collectd = Collectd('collectd/')
 
 @route('/')
 @view('index')
 def index():
     return ''
+@route('domains')
+def domain():
+	return {'domains': list(collectd.domains)}
+
+@route('domain/:domain')
+def sonde(domain):
+	return {'sondes': list(collectd.domain(domain).sondes)}
 
 @route('/data/:filename')
 def static(filename):
